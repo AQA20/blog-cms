1658e6228c6d0d4d25239a55c3bb45d1e2c9a3f7
Ahmed <ahmedqss120@gmail.com>
2025-01-10 14:39:24 +0300

    Remove useEffect for pre-filling login form in testing environment

--- components/LoginForm/LoginForm.tsx
+++ components/LoginForm/LoginForm.tsx
@@ -65,12 +65,7 @@ export function LoginForm() {
     mutation.mutate(values);
   }
 
-  useEffect(() => {
-     if (process.env.NEXT_PUBLIC_NEXT_ENV === 'testing') {
-      form.setValue('email', 'test@500kalima.com')
-      form.setValue('password', 'Qzbs2+Clw%fK4p6')
-     }
-  }, []);
+
 
   return (
     <Form {...form}>

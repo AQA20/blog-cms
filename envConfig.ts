1e25f6cc63e9a718ba87d8362020246960fad66a
Ahmed <ahmedqss120@gmail.com>
2025-01-12 18:53:51 +0300

    Fix: rename NEXT_PUBLIC_NEXT_ENV to NEXT_PUBLIC_NEXT_ENVIRONMENT in envConfig and example.env

--- envConfig.ts
+++ envConfig.ts
@@ -1,11 +1,11 @@
 import { config } from 'dotenv';
 
-if (process.env.NEXT_PUBLIC_NEXT_ENV !== 'production') {
+if (process.env.NEXT_PUBLIC_NEXT_ENVIRONMENT !== 'production') {
   config();
 }
 
 const envConfig = {
-  NEXT_ENV: process.env.NEXT_PUBLIC_NEXT_ENV as string,
+  NEXT_ENV: process.env.NEXT_PUBLIC_NEXT_ENVIRONMENT as string,
   WEB_URL: process.env.NEXT_PUBLIC_WEB_URL as string,
   API_URL: process.env.NEXT_PUBLIC_API_URL as string,
   EMAIL: process.env.EMAIL as string,
